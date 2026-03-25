139071934bf94a3de22a9f54aee173599c78be52
Sergei Grichine <msghub@hotmail.com>
2026-03-25 10:21:42 -0500

    WIP: parameters drafted

--- launch/inference_stereo.launch.py
+++ launch/inference_stereo.launch.py
@@ -26,14 +26,15 @@ def generate_launch_description():
             'verbose': True,        # If true - print debug info.
             'bind_ip': "0.0.0.0",
             'port': 5005,
-            'cloud_topic': "/stereo/sparse_cloud",
+            'image_topic': "camera/image_raw",
+            'cloud_topic': "stereo/sparse_cloud",
+            'detection_topic': 'image_inference_detections',
             'frame_id': "stereo_camera",
             'ticker_interval_sec': 0.1,   # 10 Hz UDP socket poll timer
             'socket_timeout_sec': 0.0,    # 0 for non-blocking
             'log_every_n_packets': 10,    # 0 for no log
             'color_patch_fraction': 0.5,  # center patch size relative to cell
             'use_mean_color': True,
-            'image_topic': "camera/image_raw",
             'tcp_host': "jetson.local",
             'tcp_port': 5006,
             'request_image_every_sec': 0.5,
@@ -69,10 +70,51 @@ def generate_launch_description():
                         '--frame-id', 'map', # Parent frame ID
                         '--child-frame-id', 'stereo_camera' # Child frame ID
                     ]
-   )
+    )
+
+    # Perception adapter node
+    perception_adapter_node = Node(
+        package='ros2_image_inference',
+        executable='perception_adapter',
+        name='perception_adapter',
+        output='screen',
+        parameters=[{
+            "verbose": True,        # If true - print debug info about recognized and passed detections.
+            'ticker_interval_sec': 0.1,
+            'detection_topic': 'image_inference_detections',
+            #'face_detected_sound': 'my_face.wav',
+            'face_detected_text': 'I see you!',
+            'min_confidence': 0.6,
+            'face_cooldown_sec': 2.0,
+            'gesture_cooldown_sec': 1.0,
+            'camera_center_x': 320.0,  # Assuming 640x480 input images. Adjust if different.
+            'target_label': 'person',  # The "face detected" logic is applied only to this object class.
+            # Optional mapping of incoming class labels to "normalized class labels". Normally is not needed:
+            'label_map_json': '{}',  # example: {"0":"person","1":"cat","cup_small":"cup"}',
+            # Optional JSON string mapping object labels to "gestures". If not set, capitalized label will be used:
+            'gesture_map_json': '{"bottle":"STOP", "cup":"OK", "banana":"LIKE", "cat":"MEOW", "dog":"WOOF"}',
+            # Optional JSON string mapping gestures/commands to speech output. If not set, the gesture name will be used:
+            'speech_map_json': '{"STOP":"Stop immediately!","OK":"good stuff!","scissors":"Careful, sharp!","FIRE_HYDRANT":"Fire hydrant - do not park!"}',
+        }]
+        # parameters=[params_file]  # Load params from YAML instead
+    )
+
+    detections_visualization_node = Node(
+        package='detection_visualizer',
+        executable='detection_visualizer',
+        name='detection_visualizer',
+        output='screen',
+        remappings=[
+            ('~/images', '/camera/image_raw'),
+            ('~/detections', '/image_inference_detections'),
+            ('~/dbg_images', '/image_inference_overlay'),
+        ]
+    )
 
     return LaunchDescription([
         inference_stereo_node,
+        perception_adapter_node,
+        detections_visualization_node,
         tf_to_map,
         rviz,
     ])
