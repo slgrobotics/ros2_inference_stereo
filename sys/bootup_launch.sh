f3a5379c6450066d7e0f9ee1d5e4f9802263576a
Sergei Grichine <msghub@hotmail.com>
2026-04-05 10:31:07 -0500

    Update bootup_launch.sh

--- sys/bootup_launch.sh
+++ sys/bootup_launch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# ~/launch/bootup_launch.sh
+# see https://github.com/slgrobotics/ros2_inference_stereo/blob/main/README.md#create-a-linux-service-for-on-boot-autostart
+
 source /opt/ros/jazzy/setup.bash
 
 cd /home/ros/robot_ws
